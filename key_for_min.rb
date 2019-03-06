17b07de85f6eb167efde6bb189ccedc568ba5321
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-06 10:08:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,11 @@
 def key_for_min_value(name_hash)
   low_key = nil
   low_value = nil
- name_hash.each do |k, v|
+  name_hash.each do |k, v|
     if low == nil || low_key > value
       low_value = value
       low_key = key
     end
   end
   low_name
+end
